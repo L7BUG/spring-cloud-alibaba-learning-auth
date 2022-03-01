be8dd492f3934b4d8a906d8bd9179a27f5edde66
L7-BUG <ll789@gmail.com>
2022-03-01 21:59:42 +0800

    init

---
+++ src/main/java/com/byaoh/cloud/auth/config/etc/SecurityConstants.java
@@ -0,0 +1,13 @@
+package com.byaoh.cloud.auth.config.etc;
+/**
+ * 认证常量
+ *
+ * @date 2022/3/1 下午9:11
+ * @author l
+ */
+public interface SecurityConstants {
+	/**
+	 * jwt发行人
+	 */
+	String ISS = "l";
+}
