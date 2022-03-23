ae53910479679785c0ef704f77e99a5f9c5f9210
L7-BUG <ll789@gmail.com>
2022-03-23 19:08:27 +0800

    新增两个do对象

--- src/main/java/com/byaoh/cloud/auth/domain/dataobj/MenuDo.java
+++ src/main/java/com/byaoh/cloud/auth/domain/dataobj/MenuDo.java
@@ -1,10 +1,13 @@
 package com.byaoh.cloud.auth.domain.dataobj;
 
+import java.io.Serializable;
+
 /**
  * 菜单
  *
  * @author l
  * @date 2022/3/23 下午6:29
  */
-public class MenuDo {
+public class MenuDo implements Serializable {
+	private static final long serialVersionUID = 1410880265808434184L;
 }
---
+++ src/main/java/com/byaoh/cloud/auth/domain/dataobj/RoleMenuDo.java
@@ -0,0 +1,13 @@
+package com.byaoh.cloud.auth.domain.dataobj;
+
+import java.io.Serializable;
+
+/**
+ * 角色菜单关联
+ *
+ * @author l
+ * @date 2022/3/23 下午7:07
+ */
+public class RoleMenuDo implements Serializable {
+	private static final long serialVersionUID = 807967596821537082L;
+}
